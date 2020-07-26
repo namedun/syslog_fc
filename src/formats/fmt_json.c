namedun/syslog_fc

commit c58676f22e5cb990c53bdffad81dd7aa884d73eb

json: Fix ESC sequences in JSON output
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

CMakeLists.txt
@@ -2,7 +2,7 @@ cmake_minimum_required(VERSION 2.6)
 
 PROJECT(syslog_fc C)
 INCLUDE(GNUInstallDirs)
-SET(SYSLOG_FC_VERSION 0.9.2)
+SET(SYSLOG_FC_VERSION 0.9.3)
 
 ADD_DEFINITIONS(-Os -Wall -Werror --std=gnu99 -D_GNU_SOURCE)
 ADD_DEFINITIONS(-DSYSLOG_FC_VERSION="${SYSLOG_FC_VERSION}")

src/formats/fmt_json.c
@@ -32,6 +32,7 @@ static void fmt_json_output_encoded(const char *string)
 			case '\t': fputs("\\t",  stdout); break;
 			case '\\': fputs("\\\\", stdout); break;
 			case '"' : fputs("\\\"", stdout); break;
+			case 0x1b: fputs("\\033", stdout); break;
 
 			default:
 				fputc(*p, stdout);

commit 9c11c8eeb05a5b289aa68a32207d6fe7b83ae837

json: Output escape (0x1b) as "\\033" in JSON output
Fixes: c58676f ("json: Fix ESC sequences in JSON output")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

CMakeLists.txt
@@ -2,7 +2,7 @@ cmake_minimum_required(VERSION 2.6)
 
 PROJECT(syslog_fc C)
 INCLUDE(GNUInstallDirs)
-SET(SYSLOG_FC_VERSION 0.9.3)
+SET(SYSLOG_FC_VERSION 0.9.4)
 
 ADD_DEFINITIONS(-Os -Wall -Werror --std=gnu99 -D_GNU_SOURCE)
 ADD_DEFINITIONS(-DSYSLOG_FC_VERSION="${SYSLOG_FC_VERSION}")

src/formats/fmt_json.c
@@ -32,7 +32,7 @@ static void fmt_json_output_encoded(const char *string)
 			case '\t': fputs("\\t",  stdout); break;
 			case '\\': fputs("\\\\", stdout); break;
 			case '"' : fputs("\\\"", stdout); break;
-			case 0x1b: fputs("\\033", stdout); break;
+			case 0x1b: fputs("\\\\033", stdout); break;
 
 			default:
 				fputc(*p, stdout);